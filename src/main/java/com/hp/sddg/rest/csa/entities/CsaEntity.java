d6c4289307ab47f15e4b9a685076b43573e31469
pe-pan <pe.pan@centrum.cz>
2015-05-19 11:27:46 +0000

    REQ: entity properties are being stored in a map and json generated on the fly

--- src/main/java/com/hp/sddg/rest/csa/entities/CsaEntity.java
+++ src/main/java/com/hp/sddg/rest/csa/entities/CsaEntity.java
@@ -1,21 +1,33 @@
 package com.hp.sddg.rest.csa.entities;
 
 import com.hp.sddg.rest.common.entities.Entity;
+import com.jayway.jsonpath.InvalidPathException;
 import com.jayway.jsonpath.JsonPath;
+import net.minidev.json.JSONArray;
+import org.apache.log4j.Logger;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * Created by panuska on 2.10.14.
  */
 public class CsaEntity extends Entity {
+    private static Logger log = Logger.getLogger(CsaEntity.class.getName());
+    protected Map<String, String> properties;
+    private boolean isDirty;
     protected String json;
 
     public CsaEntity(Object json) {
         this.json = (String)json;
+        properties = new HashMap<>();
+        isDirty = false;
     }
 
     @Override
     protected void init(Object o) {
         this.json = (String) o;
+        isDirty = false;
     }
 
     public String getId() {
@@ -31,33 +43,85 @@ public class CsaEntity extends Entity {
 
     @Override
     public String getProperty(String key) {
-        if (key.equals("id")) {
-            return getId();
+        String value = properties.get(key);
+        if (value == null) {
+            try {
+                Object o = JsonPath.read(json, "$."+key);
+                if (o instanceof JSONArray) {
+                    JSONArray a = (JSONArray) o;
+                    if (a.size() != 1) return null;
+                    value = a.get(0).toString();
+                } else {
+                    value = o == null ? null : o.toString();
+                }
+            } catch (InvalidPathException e) {
+                log.debug("Invalid Path $." + key, e);
+            }
+            if (value != null) {
+                properties.put(key, value);
+            }
         }
-        Object value = JsonPath.read(json, "$."+key);
-        return value == null ? "null" : value.toString().replace('\n', ' '); //todo remove new lines
+        if (value != null) value = value.replace('\n', ' ');    // todo remove new lines
+        return value;
     }
 
     @Override
     public void setProperty(String key, String value) {
-        throw new IllegalStateException("Not implemented!");
+        properties.put(key, value);
+        isDirty = true;
     }
 
     @Override
     public boolean isDirty(String key) {
         //todo not properly implemented
-        return false;
+        return isDirty;
     }
 
     @Override
     public boolean isDirty() {
-        //todo not properly implemented
-        return false;
+        return isDirty;
     }
 
     @Override
     public String toJson() {
+        if (isDirty) {
+            json = "{ "
+                    +propertiesToJson()
+                    + " }";
+            isDirty = false;
+        }
         return json;
+    }
+
+    protected String propertiesToJson() {
+        Map<String, String> fields = new HashMap<>();
+        StringBuilder b = new StringBuilder();
+        for (String key : properties.keySet()) {
+            String value = properties.get(key);
+            if (key.startsWith("field_")) {
+                fields.put(key, value);
+            } else {
+                b.append("\"").append(key).append("\" : \"").append(value).append("\", ");
+            }
+        }
+        b.deleteCharAt(b.length()-1);  // remove the very last comma
+        b.deleteCharAt(b.length()-1);
+
+        if (!fields.isEmpty()) {
+            b.append(", \"fields\" : {");
+            for (String key : fields.keySet()) {
+                String value = fields.get(key);
+                if (value.equals("true") || value.equals("false")) {
+                    b.append("\"").append(key.substring("field_".length())).append("\" : ").append(value).append(", ");
+                }
+                b.append("\"").append(key.substring("field_".length())).append("\" : \"").append(value).append("\", ");
+            }
+            b.deleteCharAt(b.length()-1);  // remove the very last comma
+            b.deleteCharAt(b.length()-1);
+            b.append("}");
+        }
+
+        return b.toString();
     }
 
     @Override
